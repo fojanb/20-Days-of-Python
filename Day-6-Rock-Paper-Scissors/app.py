ad9ec350950708c02f2f624e0c2ed3763cc49847
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-05-08 10:07:53 -0700

    Debug random number

--- Day-6-Rock-Paper-Scissors/app.py
+++ Day-6-Rock-Paper-Scissors/app.py
@@ -1,8 +1,9 @@
 from random import randint
+from colors import BRIGHT_CYAN,BRIGHT_MAGENTA,BLUE,RED,GREEN,RESET
+
 options = ["Rock","Scissors","Paper"]
-computer = options[randint(0,2)]
 def get_user_input():
-    player_turn = input(" Enter 'r' for Rock\n Enter 'p' for Paper\n Enter 's' for Scissors\n >> ")
+    player_turn = input(BRIGHT_CYAN + " Enter 'r' for Rock\n Enter 'p' for Paper\n Enter 's' for Scissors\n >> " + RESET)
     match(player_turn.upper()):
         case 'R':
             return options[0]
@@ -11,33 +12,39 @@ def get_user_input():
         case 'P':
             return options[2]
         case _:
-            print("Not a valid input !")
-            exit(0);
+            print(RED + "Not a valid input !" + RESET)
+            try_again = input("Would you like to try again? (y/n)\n")
+            if(try_again.upper() == 'Y'):
+                start()
+            else:
+                print(BRIGHT_MAGENTA + "Not a problem ! Have some rest." + RESET)
+                exit(0);
 
-def compare_players_input(player):
+def compare_players_input(player,computer):
     if(player == computer):
+        print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
         print("Tie !")
     elif (player == "Rock"):
         if(computer == "Paper"):
-            print("Computer:", computer," - ","You:", player)
-            print("You lose!", computer, "covers", player)
+            print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
+            print(RED + "You lose... "+computer+ " covers "+ player + RESET)
         else:
-            print("Computer:", computer," - ", "You:", player)
-            print("You win!", player, "breaks", computer)
+            print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
+            print(GREEN + "You win! "+player+ " breaks "+ computer+RESET)
     elif (player == "Paper"):
         if(computer == "Rock"):
-            print("Computer:", computer," - ", "You:", player)
-            print("You Win!", player, "covers", computer)
+            print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
+            print(GREEN + "You Win! "+ player+ " covers "+ computer+RESET)
         else:
-            print("Computer:", computer," - ", "You:", player)
-            print("You lose!", computer, "cuts", player)
+            print(BLUE + "Computer: "+ computer +" - "+"You: "+ player + RESET)
+            print(RED + "You lose... "+ computer + " cuts "+ player+RESET)
     elif (player == "Scissors"):
         if(computer == "Rock"):
-            print("Computer:", computer," - ", "You:", player)
-            print("You lose!", computer, "breaks", player)
+            print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
+            print(RED+"You lose... "+ computer+ " breaks "+ player+RESET)
         else:
-            print("Computer:", computer," - ", "You:", player)
-            print("You win!", player, "cuts", computer)
+            print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
+            print(GREEN + "You win! "+ player + " cuts "+ computer +RESET)
      
 def continue_play():
     is_agree = input("Do you want to keep on playing ? (y/n)\n")
@@ -46,9 +53,10 @@ def continue_play():
     else:
         print("Ok, See you then and bye bye.")
         
-def start():    
+def start():   
+    computer = options[randint(0,2)] 
     player = get_user_input()
-    compare_players_input(player)
+    compare_players_input(player,computer)
     continue_play()
     
 start()

3378cbd51ce41da8e8b3367a422e9498e4589f3f
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-05-08 14:37:13 -0700

    init

--- Day-7-Web-Scraper/README.md
+++ Day-7-Web-Scraper/README.md
@@ -15,14 +15,16 @@ Web scraping, the process of extracting data from websites, has emerged as a pow
 Essential Packages and Tools for Python Web Scraping
 The latest version of Python, offers a rich set of tools and libraries specifically designed for web scraping, making it easier than ever to retrieve data from the web efficiently and effectively.
 
-1. Requests Module
--The requests library is used for making HTTP requests to a specific URL and returns the response. Python requests provide inbuilt functionalities for managing both the request and response.
-2. BeautifulSoup Library
-3. Selenium
-4. Lxml 
-5. Urllib Module
-6. PyautoGUI
-7. Schedule
+### 1. Requests Module
+-find the API Documentation here : https://requests.readthedocs.io/en/latest/api/
+### 2. BeautifulSoup Library
+-Beautiful Soup is a Python library that works with a parser to extract data from HTML and can turn even invalid markup into a parse tree. However, this library is only designed for parsing and cannot request data from web servers in the form of HTML documents/files. For this reason, it's mostly used alongside the Python Requests Library. Note that Beautiful Soup makes it easy to query and navigate the HTML but still requires a parser. The following example demonstrates the use of the html.parser module, which is part of the Python Standard Library. 
+### 3. Selenium
+### 4. Lxml 
+### 5. Urllib Module
+### 6. PyautoGUI
+### 7. Schedule
+### 8. Pandas 
 
  
 ### Running and test
--- Day-7-Web-Scraper/app.py
+++ Day-7-Web-Scraper/app.py
@@ -1,62 +1,8 @@
-from random import randint
-from colors import BRIGHT_CYAN,BRIGHT_MAGENTA,BLUE,RED,GREEN,RESET
+from requests import get
+from bs4 import BeautifulSoup
 
-options = ["Rock","Scissors","Paper"]
-def get_user_input():
-    player_turn = input(BRIGHT_CYAN + " Enter 'r' for Rock\n Enter 'p' for Paper\n Enter 's' for Scissors\n >> " + RESET)
-    match(player_turn.upper()):
-        case 'R':
-            return options[0]
-        case 'S':
-            return options[1]
-        case 'P':
-            return options[2]
-        case _:
-            print(RED + "Not a valid input !" + RESET)
-            try_again = input("Would you like to try again? (y/n)\n")
-            if(try_again.upper() == 'Y'):
-                start()
-            else:
-                print(BRIGHT_MAGENTA + "Not a problem ! Have some rest." + RESET)
-                exit(0);
 
-def compare_players_input(player,computer):
-    if(player == computer):
-        print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
-        print("Tie !")
-    elif (player == "Rock"):
-        if(computer == "Paper"):
-            print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
-            print(RED + "You lose... "+computer+ " covers "+ player + RESET)
-        else:
-            print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
-            print(GREEN + "You win! "+player+ " breaks "+ computer+RESET)
-    elif (player == "Paper"):
-        if(computer == "Rock"):
-            print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
-            print(GREEN + "You Win! "+ player+ " covers "+ computer+RESET)
-        else:
-            print(BLUE + "Computer: "+ computer +" - "+"You: "+ player + RESET)
-            print(RED + "You lose... "+ computer + " cuts "+ player+RESET)
-    elif (player == "Scissors"):
-        if(computer == "Rock"):
-            print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
-            print(RED+"You lose... "+ computer+ " breaks "+ player+RESET)
-        else:
-            print(BLUE + "Computer:"+ computer+" - "+"You:"+ player + RESET)
-            print(GREEN + "You win! "+ player + " cuts "+ computer +RESET)
-     
-def continue_play():
-    is_agree = input("Do you want to keep on playing ? (y/n)\n")
-    if(is_agree.upper() == 'Y'):
-       start()
-    else:
-        print("Ok, See you then and bye bye.")
-        
-def start():   
-    computer = options[randint(0,2)] 
-    player = get_user_input()
-    compare_players_input(player,computer)
-    continue_play()
-    
-start()
+response = get("https://github.com/fojanb?tab=repositories")
+# print(response.text)
+my_soup = BeautifulSoup(response.text,'html.parser')
+print(my_soup.title)
